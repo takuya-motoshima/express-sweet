dc8e52fea3ccc95cdf9443e6f6e9af758d6f1a5e
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2021-05-19 18:04:03 +0900

    Fixed model association doc.

--- docs/scripts/docs.js
+++ docs/scripts/docs.js
@@ -1,28 +1,23 @@
 (() => {
-  // Highlight the code block..
-  document.querySelectorAll('pre code').forEach((block) => {
-    hljs.highlightBlock(block);
-  });
+  // Highlight the code block.
+  document.querySelectorAll('pre code').forEach((block) => 
+    hljs.highlightBlock(block));
 
   // Overlay when the sidebar opens.
   const sidebar = document.querySelector('#sidebar');
   const body = document.body;
-  const sidebarCollapse = new bootstrap.Collapse(sidebar, {
-    toggle: false
-  });
+  const sidebarCollapse = new bootstrap.Collapse(sidebar, {toggle: false});
 
-  sidebar.addEventListener('shown.bs.collapse', () => {
-    body.setAttribute('data-offcanvas-sidebar', 'on');
-  });
+  // Show overlay when opening sidebar.
+  sidebar.addEventListener('shown.bs.collapse', () => 
+    body.setAttribute('data-offcanvas-sidebar', 'on'));
 
-  // Stop overlay when sidebar closes.
-  sidebar.addEventListener('hidden.bs.collapse', () => {
-    body.removeAttribute('data-offcanvas-sidebar');
-  });
+  // Hide the overlay when you close the sidebar.
+  sidebar.addEventListener('hidden.bs.collapse', () => 
+    body.removeAttribute('data-offcanvas-sidebar'));
 
   // Close the sidebar when the overlay is clicked.
   const sidebarOverlay = document.querySelector('.sidebar-overlay');
-  sidebarOverlay.addEventListener('click', () => {
-    sidebarCollapse.hide();
-  });
+  sidebarOverlay.addEventListener('click', () => 
+    sidebarCollapse.hide());
 })();
