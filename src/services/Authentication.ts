0308d61f6ec4c1960354001b8be39e76c0068154
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2021-05-24 22:15:13 +0900

    User authentication document completed.

--- src/services/Authentication.ts
+++ src/services/Authentication.ts
@@ -32,10 +32,8 @@ export default class {
    * Log out the user.
    * 
    * @param {express.Request}      req  The req object represents the HTTP request and has properties for the request query string, parameters, body, HTTP headers, and so on.
-   * @param {express.Response}     res  The res object represents the HTTP response that an Express app sends when it gets an HTTP request.
-   * @param {express.NextFunction} next A function that can access the next middleware function in the application's request-response cycle.
    */
-  public static logout(req: express.Request, res: express.Response, next: express.NextFunction): void {
+  public static logout(req: express.Request): void {
     req.logout();
   }
 
--- types/services/Authentication.d.ts
+++ types/services/Authentication.d.ts
@@ -16,10 +16,8 @@ export default class {
      * Log out the user.
      *
      * @param {express.Request}      req  The req object represents the HTTP request and has properties for the request query string, parameters, body, HTTP headers, and so on.
-     * @param {express.Response}     res  The res object represents the HTTP response that an Express app sends when it gets an HTTP request.
-     * @param {express.NextFunction} next A function that can access the next middleware function in the application's request-response cycle.
      */
-    static logout(req: express.Request, res: express.Response, next: express.NextFunction): void;
+    static logout(req: express.Request): void;
     /**
      * Redirects to the page that responds immediately after successful authentication set in "success_redirect" of "config/authentication.js".
      *
