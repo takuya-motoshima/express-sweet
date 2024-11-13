b52246eaefbe5b1ff7ab2f59635ae5f88b0bfe82
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2024-11-13 10:17:19 +0900

    Fix JS comments.

--- src/interfaces/BasicConfig.ts
+++ src/interfaces/BasicConfig.ts
@@ -68,11 +68,11 @@ export default interface BasicConfig {
    * @example
    * hook_handle_error: (err, req, res, next) => {
    *   if (err.status === 404)
-   *     // If the URL cannot be found, a 404 error screen (views/error-404.hbs) is displayed.
-   *     res.render('error-404');
+   *     // If the URL cannot be found, a 404 error screen (views/errors/404.hbs) is displayed.
+   *     res.render('errors/404');
    *   else
-   *     // For other errors, unknown error screen (views/error-unknown.hbs) is displayed.
-   *     res.render('error-unknown');
+   *     // For other errors, unknown error screen (views/error/500.hbs) is displayed.
+   *     res.render('error/500');
    * },
    */
   hook_handle_error?: (err: any, req: express.Request, res: express.Response, next: express.NextFunction) => void,
